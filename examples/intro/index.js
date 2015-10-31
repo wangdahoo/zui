5a5619ef506a7464703c52b51b5e352e64d878a6
wangdahoo <157195705@qq.com>
2015-10-31 17:55:41 +0800

    update

--- examples/intro/index.js
+++ examples/intro/index.js
@@ -1,5 +1,113 @@
 $(function() {
 
+    /**
+     * Guide v1.0.0
+     *
+     * 1. Core
+     *
+     * zui.Engine()
+     *  - Use this method to create global app instance, who is responsible for routes in a Single Page Application.
+     *
+     *  eg.
+     *      var app = zui.Engine();
+     *
+     *      app.init({                          // init engine with a routes map
+     *          routes: {                       // { [String routeName]:  [zui.Page page], ... }
+     *              master: masterPage,
+     *              detail: detailPage,
+     *          },
+     *
+     *          defaultRoute: 'master'
+     *      });
+     *
+     *      app.navigate('master');             // navigate to a page which by route 'master'.
+     *
+     *      app.navigate('master', {            // navigate to a page with routeName & routeParams
+     *          id: 123,
+     *          message: 'Come back!'
+     *      });
+     *
+     *      // todo: Deprecated for translate3d issue on some versions of ios Safari.
+     *      // app.navigate('master', 'forward')   // navigate to a page with routeName & transition.
+     *
+     *
+     *      // app.navigate('master', {            // navigate to a page with routeName, routeParams & transition.
+     *      //     id: 123,
+     *      //     msg: 'May I have a cigarette?'
+     *      // }, 'back');
+     *
+     *      app.start();                        // call navigate() with the defaultRoute.
+     *
+     *      app.back();                         // navigate to the last page by history.
+     *
+     *      app.getCurrentRoute();              // return current route.
+     *
+     *      var params = app.GET;               // get all parameters from URL.
+     *
+     *      var count = 0;
+     *      var tid = setInterval(function() {
+     *          console.log(count++);
+     *      }, 1000);
+     *      app.addTimer(tid);                  // mark down a timer id, and the timer will be eliminated automatically
+     *                                          // when you call navigate() to next page.
+     *
+     *      app.deleteTimer(tid);               // eliminate a timer you marked down.
+     *
+     *      app.clearTimers();                  // eliminate all timers you marked down.
+     *
+     * zui.Page([Object options])
+     *  - Use this method to create a Page View.
+     *
+     *  eg.
+     *      var myPage = zui.Page({
+     *          title: 'Hey Jude',              // title on page's navigation bar
+     *
+     *          backButton: function() {        // click callback for a back button, optional. leave the option undefined if you don't need a back button on the navigation bar.
+     *              // do your magic here.
+     *          },
+     *
+     *          template: $('#tpl-homepage'),   // a underscore template for page content rendering.
+     *
+     *          model: {                        // data model for template.
+     *              author: 'The Beatles',
+     *              year: '1968',
+     *              style: 'Rock, Pop'
+     *          },
+     *
+     *          ready: function () {            // a callback who execute immediately after page rendering.
+     *              // do your magic here.
+     *          }
+     *      })
+     *
+     * zui.View()
+     *  - TBD
+     *
+     * 2. Components
+     *
+     * zui.showLoading([Object options])
+     *
+     *  eg.
+     *      zui.showLoading({
+     *          message: 'Loading...',
+     *          duration: 2000
+     *      })
+     *
+     * zui.hideLoading();
+     *
+     *  eg.
+     *      zui.hideLoading();
+     *
+     * zui.alert([String message])
+     *  - todo
+     *
+     * zui.confirm([Object options])
+     *  - todo
+     *
+     * zui.swipe([Object options])
+     *  - TBD
+     *
+     */
+
     /* Step 1
      * Create an app engine */
 
@@ -32,7 +140,7 @@ $(function() {
                 if (route == 'loading') {
                     zui.showLoading('加载中');
                 } else {
-                    app.navigate(route, 'slide');
+                    app.navigate(route);
                 }
             })
 
@@ -66,15 +174,13 @@ $(function() {
     pages.grid = zui.Page({
         title: 'Grid',
         backButton: function(e) {
-            app.navigate('home', 'back');
+            app.back();
         },
         template: $('#tpl-grid').html()
     });
 
     /* Step 3
-     * Setup app engine's routes with pages, and then call engine's start() method.
-     *
-     * Have Fun ;) */
+     * Setup app engine's routes with pages, and then call engine's start() method. */
 
     app.init({
         routes: {
@@ -89,5 +195,5 @@ $(function() {
     })
         .start(); // Start App
 
-    window.app = app;
+    window.app = app; // Expose the app instance to window. Have Fun ;)
 });
